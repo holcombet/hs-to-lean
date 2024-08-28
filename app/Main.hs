87b9649960e0d4e03f0afb91162933665a56d82f
Alexander Kurz <alexhkurz@gmail.com>
2024-08-28 15:52:07 -0700

    feat: Add prettyPrint function and clean up commented code in Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -18,7 +18,13 @@ import "ghc" GHC.Driver.Phases (Phase(Cpp))
 import "ghc" GHC.Types.SourceFile (HscSource(HsSrcFile))
 import "ghc" GHC.Unit.Module 
 
-
+prettyPrint :: String -> String
+prettyPrint = unlines . snd . foldl processChar (0, []) where
+    processChar (indent, lines) char
+        | char == '{' = (indent + 1, lines ++ [replicate indent ' ' ++ [char]])
+        | char == '}' = (indent - 1, lines ++ [replicate (indent - 1) ' ' ++ [char]])
+        | otherwise = (indent, init lines ++ [last lines ++ [char]])
+        
 -- runs but doesn't parse --
 main :: IO ()
 main = do
@@ -35,68 +41,4 @@ main = do
       let ast = pm_parsed_source parsedModule
       let prettyAst = prettyPrint $ gshow ast
       liftIO $ putStrLn prettyAst
-
-
--- doesnt work --
-
--- main :: IO ()
--- main = do
---   defaultErrorHandler defaultFatalMessager defaultFlushOut $ do
---     runGhc (Just libdir) $ do
---       dflags <- getSessionDynFlags
---       setSessionDynFlags dflags
---       target <- guessTarget "/Users/tallyholcombe/Documents/Thesis/hs-to-lean/src/Test.hs" Nothing (Just (Cpp HsSrcFile))
---       setTargets [target]
---       load LoadAllTargets
---       let moduleName = takeBaseName "/Users/tallyholcombe/Documents/Thesis/hs-to-lean/src/Test.hs"
---       modSum <- getModSummary $ mkModuleName moduleName
---       parsedModule <- GHC.parseModule modSum
-      
-
---       tcheck <- GHC.typecheckModule p
---       dsugar <- GHC.desugarModule tcheck
---       loadM <- GHC.loadModule dsugar
---       n <- GHC.getNamesInScope
---       c <- return $ GHC.coreModule dsugar 
-
---       g <- GHC.getModuleGraph 
---       mapM showModule g 
---       putStrLn $ (parsedSource dsugar, "/n-----/n",  typecheckedSource dsugar )
-
-
-
-
-
-
-
-
-
--- main :: IO ()
--- main = do
---    res <- example
---    str <- runGhc (Just libdir) $ do
---       dflags <- getSessionDynFlags
---       return $ showSDoc dflags $ ppr res
---    putStrLn str
--- example = 
---   defaultErrorHandler defaultFatalMessager defaultFlushOut $ do
---     runGhc (Just libdir) $ do
---         dflags <- getSessionDynFlags
---         let dflags' = foldl xopt_set dflags
---                             [Opt_Cpp, Opt_ImplicitPrelude, Opt_MagicHash]
---         setSessionDynFlags dflags'
---         target <- guessTarget "/Users/tallyholcombe/Documents/Thesis/hs-to-lean/src/Test.hs" Nothing
---         setTargets [target]
---         load LoadAllTargets
---         modSum <- getModSummary $ mkModuleName "Test"
---         p <- parseModule modSum
---         t <- typecheckModule p
---         d <- desugarModule t
---         l <- loadModule d
---         n <- getNamesInScope
---         c <- return $ coreModule d
- 
---         g <- getModuleGraph
---         mapM showModule g     
---         return $ (parsedSource d,"/n-----/n",  typecheckedSource d)
 
