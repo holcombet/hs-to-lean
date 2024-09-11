78d0e768ec659dda16e52e3abaafc78c5d2d402b
Alexander Kurz <alexhkurz@gmail.com>
2024-09-11 16:18:41 -0700

    insert works now

--- test-ak.lean
+++ test-ak.lean
@@ -1,12 +1,10 @@
 def myMax (a b : Nat) : Nat :=
   if a > b then a else b
 
-def insert (a : Nat) (l : List Nat) : List Nat :=
-  if l.isEmpty
-    then [a]
-    else if a <= l.head!
-      then a :: l
-      else l.head! :: (insert a l.tail!)
-termination_by _ l => l.length
+def myInsert (a : Nat) : (List Nat) -> List Nat
+| [] => [a]
+| x :: xs => if a <= x
+                then a :: x :: xs
+                else x :: myInsert a xs
 
-def main : IO Unit := IO.println (myMax 10 20)
+def main : IO Unit := IO.println (myInsert 2 [1,3])
--- test.lean
+++
@@ -1,4 +0,0 @@
-import init.data.nat.basic
-
-def max (a b : ℕ) : ℕ :=
-if a > b then a else b
