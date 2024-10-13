55eea9465a30a77837a6b22464f821e00360d0c6
holcombet <holcombe.t.23@gmail.com>
2024-10-12 17:02:28 -0700

    lean function name binding

--- src/TranslateHaskell.hs
+++ src/TranslateHaskell.hs
@@ -23,6 +23,36 @@ import "ghc" GHC.Types.SourceText
 
 
 translateToLean :: ParsedSource -> IO()
-translateToLean ast = liftIO $ writeFile "src/result.txt" (gshow ast)
+translateToLean ast = do
+    let parsedAst =  map (unXRec @(GhcPass 'Parsed)) $ hsmodDecls $ unLoc $ ast
+
+    liftIO $ writeFile "src/result.txt" (unlines $ map prettyLeanDecl parsedAst)
+
+-- translateToLean ast = 
+
+prettyLeanDecl :: HsDecl GhcPs -> String
+prettyLeanDecl = \case
+    TyClD _ decl -> case decl of
+        SynDecl _ name tyVar fix rhs -> "Not implemented"
+        _ -> "Not implemented"
+    ValD _ decl -> case decl of
+        FunBind _ name matches _ -> "Not implemented\n"   -- requires Pat and GRHSs 
+                                                          -- NOTE: remove newline 
+            -- let funName = (occNameString . occName . unXRec @(GhcPass 'Parsed)) names
+            --     matchStrings = map (\L (L _ (Match _ c pats body)) ->
+            --         let argStrings = "\t | " ++ unwords (map prettyLeanPat pats)
+            --             bodyString = prettyLeanGRHSs body
+            --         in argStrings ++ " = " ++ bodyString) (unLoc $ mg_alts matches)
+            -- in unlines matchStrings
+        PatBind _ pat rhs _ -> "Not implemented"    -- requires Pat
+        VarBind _ var rhs -> "Not implemented"      -- requires LHsExpr
+        _ -> "Not implemented"
+    SigD _ decl -> case decl of
+        TypeSig _ names typ -> "def " ++ unwords (map (occNameString . occName . unXRec @(GhcPass 'Parsed)) names) ++ " : " ++ "Not implemented"    -- requires LHsSigWcType
+        PatSynSig _ names typ -> "def " ++ unwords (map (occNameString . occName . unXRec @(GhcPass 'Parsed)) names) ++ " : " ++ "Not implemented"  -- requires LHsSigType
+        _ -> "Not implemented"
+    _ -> "Not implemented"
 
 
+
+main = putStrLn "Done."
