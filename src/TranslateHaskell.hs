a6a784a2d36898b82dadbd37f1d4c9493bb98925
holcombet <holcombe.t.23@gmail.com>
2024-10-12 18:13:51 -0700

    implemented some function binding and pattern matching

--- src/TranslateHaskell.hs
+++ src/TranslateHaskell.hs
@@ -36,14 +36,14 @@ prettyLeanDecl = \case
         SynDecl _ name tyVar fix rhs -> "Not implemented"
         _ -> "Not implemented"
     ValD _ decl -> case decl of
-        FunBind _ name matches _ -> "Not implemented\n"   -- requires Pat and GRHSs 
+        FunBind _ name matches _ -> --"Not implemented\n"   -- requires Pat and GRHSs 
                                                           -- NOTE: remove newline 
-            -- let funName = (occNameString . occName . unXRec @(GhcPass 'Parsed)) names
-            --     matchStrings = map (\L (L _ (Match _ c pats body)) ->
-            --         let argStrings = "\t | " ++ unwords (map prettyLeanPat pats)
-            --             bodyString = prettyLeanGRHSs body
-            --         in argStrings ++ " = " ++ bodyString) (unLoc $ mg_alts matches)
-            -- in unlines matchStrings
+            let funName = (occNameString . occName . unXRec @(GhcPass 'Parsed)) name
+                matchStrings = map (\(L _ (Match _ c pats body)) ->
+                    let argStrings = "\t| " ++ unwords (map prettyLeanPat pats)
+                        bodyString = "Not implemented" -- prettyLeanGRHSs body
+                    in argStrings ++ " => " ++ bodyString) (unLoc $ mg_alts matches)
+            in unlines matchStrings
         PatBind _ pat rhs _ -> "Not implemented"    -- requires Pat
         VarBind _ var rhs -> "Not implemented"      -- requires LHsExpr
         _ -> "Not implemented"
@@ -82,5 +82,44 @@ prettyLeanType = \case
     HsParTy _ typ -> "Not implemented"
     HsOpTy _ _ typ1 id typ2 -> "Not implemented"
     _ -> "Not implemented"
+
+
+prettyLeanMatchGroup :: MatchGroup GhcPs (LHsExpr GhcPs) -> String
+prettyLeanMatchGroup = \case
+    MG ext (L _ matches) _ -> unlines $ map prettyLLeanMatch matches
+
+prettyLLeanMatch :: LMatch GhcPs (LHsExpr GhcPs) -> String
+prettyLLeanMatch (L _ match) = case match of
+    Match _ _ pats body -> unwords (map prettyLeanPat pats) ++ " -> " ++ prettyLeanGRHSs body
+
+prettyLeanPat :: LPat GhcPs -> String
+prettyLeanPat (L _ pat) = case pat of
+    VarPat _ typ -> occNameString . occName . unLoc $ typ   -- might have to implement a renamer down the line...insert
+    LitPat _ pat -> "Not implemented"       -- requires HsLit
+    ListPat _ pats -> "[" ++ intercalate "," (map prettyLeanPat pats) ++ "]"        -- still have literal lists...
+    NPat _ lit _ _ -> "Not implemented"     -- literals
+    ParPat _ tokLeft pat tokRight -> "(" ++ prettyLeanPat pat ++ ")"
+    ConPat _ (L _ name) details ->
+        let conName = occNameString . occName $ name
+            patDetails = prettyLeanConPatDetails details 
+        in case details of
+            PrefixCon _ _ -> conName ++ " " ++ unwords patDetails       -- not sure what this does yet, no change
+            InfixCon _ _ -> intercalate (" " ++ "::" ++ " ") patDetails     -- for colon lists (::) but not quite right
+
+    _ -> "Not implemented"
+
+
+prettyLeanConPatDetails :: HsConPatDetails GhcPs -> [String]
+prettyLeanConPatDetails details = case details of
+    PrefixCon tyarg arg -> map prettyLeanPat arg
+    InfixCon arg1 arg2 -> [prettyLeanPat arg1, prettyLeanPat arg2]
+    
+
+prettyLeanGRHSs :: GRHSs GhcPs (LHsExpr GhcPs) -> String
+prettyLeanGRHSs (GRHSs _ grhss _) = unwords $ map prettyLeanGRHS grhss
+
+prettyLeanGRHS :: LGRHS GhcPs (LHsExpr GhcPs) -> String
+prettyLeanGRHS (L _ (GRHS _ _ body)) = "Not implemented" --prettyLHsExpr body
+
 
 main = putStrLn "Done."
