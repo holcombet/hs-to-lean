243e59f523c539a9e3a9c1a2448685f6fc8bbf9e
holcombet <holcombe.t.23@gmail.com>
2025-01-14 13:00:38 -0800

    updating repo

--- src/Calendar.hs
+++ src/Calendar.hs
@@ -1,5 +1,23 @@
 module Calendar where
 
+-- data Tree = Nil | Node Int Tree Tree
+
+-- insert :: Int -> Tree -> Tree
+-- insert x Nil = Node x Nil Nil
+-- insert x (Node a left right) = 
+--     if x < a then Node a (insert x left) right else Node a left (insert x right)
+
+-- heapify :: [Int] -> Tree
+-- heapify [] = Nil
+-- heapify (x:xs) = insert x (heapify xs)
+
+-- tree2list :: Tree -> [Int]
+-- tree2list Nil = []
+-- tree2list (Node a left right) = tree2list left ++ [a] ++ tree2list right
+
+-- heapSort = tree2list . heapify 
+
+
 
 
 add arg1 arg2 = arg1 + arg2
--- src/Test.hs
+++ src/Test.hs
@@ -5,5 +5,11 @@ recursiveAdd n 0 = n
 recursiveAdd n m = 1 + recursiveAdd n (m-1)
 
 
+newtype UserId = UserId Int deriving (Show)
 
+myId :: UserId
+myId =  UserId 10
+
+main = do 
+    print myId
 
